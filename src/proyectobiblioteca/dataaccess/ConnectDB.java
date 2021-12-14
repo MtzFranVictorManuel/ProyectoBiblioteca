8ea50be45809448361b4cbb890fb1a4cd7a94963
Saulo-Colorado <saulocolorado79@gmail.com>
2021-12-13 20:26:35 -0600

    Adding Database File Connections 2

--- src/proyectobiblioteca/dataaccess/ConnectDB.java
+++ src/proyectobiblioteca/dataaccess/ConnectDB.java
@@ -4,10 +4,45 @@
  */
 package proyectobiblioteca.dataaccess;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Saulo
  */
 public class ConnectDB {
-    
+    protected Connection connection;
+    private String url = "";
+    private String user = "";
+    private String password = ""; 
+
+    public Connection getDataBaseConnection() {
+        connection = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = (Connection) DriverManager.getConnection(url,user,password);
+        } catch(ClassNotFoundException exClassNotFoundException) {
+
+        } catch(SQLException exSQL) {
+            System.out.println("Error: " + exSQL);
+        }
+        
+        return connection;
+    }
+
+    public Connection closeDataBaseConnection(){
+        try{
+            if(connection != null){
+                if(!connection.isClosed()){
+                    connection.close();
+                }
+            }
+        }catch(SQLException exSQL){
+            System.out.println("Error: "+ exSQL);
+        }
+        
+        return connection;
+    }
 }
