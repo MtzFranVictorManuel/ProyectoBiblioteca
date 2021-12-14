ef9b10b64ec6610bc74179f823a5028519b3619e
Saulo-Colorado <saulocolorado79@gmail.com>
2021-12-13 20:25:33 -0600

    Adding Database File Connections

---
+++ src/proyectobiblioteca/dataaccess/DBConnection.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectobiblioteca.dataaccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Saulo
+ */
+public class DBConnection {
+    private static final String JDBC_USER="root";
+    private static final String JDBC_DATABASE="databasebiblioteca";
+    private static final String JDBC_PASSWORD="";
+    private static final String JDBC_URL = "jdbc:mysql://localhost/databasebiblioteca?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    
+    public static Connection getConnection(){
+        Connection connect = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        } catch (ClassNotFoundException excep) {
+            System.out.println("Error de conexion " + excep.getMessage());
+        } catch (SQLException excep) {
+            System.out.println("Error de conexion " + excep.getMessage());
+        }
+
+        return connect;
+    }
+
+    public static void close(ResultSet rSet){
+        try{
+            if(rSet != null){
+                rSet.close();
+            }
+        }
+        catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+    }
+
+    public static void close (PreparedStatement pStatement){
+        try{
+            if(pStatement != null){
+                pStatement.close();
+            }
+        }
+        catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+    }
+
+    public static void close(Connection connect){
+        try{
+            if(connect != null){
+                connect.close();
+            }
+        }
+        catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+    }
+}
