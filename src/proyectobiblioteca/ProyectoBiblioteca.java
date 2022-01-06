0908c8a8e10e0f0521851070b3b80b168be2b72e
MtzFranVictorManuel <40128870+MtzFranVictorManuel@users.noreply.github.com>
2022-01-05 22:58:39 -0600

    s
    s

--- src/proyectobiblioteca/ProyectoBiblioteca.java
+++ src/proyectobiblioteca/ProyectoBiblioteca.java
@@ -28,7 +28,7 @@ public class ProyectoBiblioteca extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {      
         launch(args);
     }
     
